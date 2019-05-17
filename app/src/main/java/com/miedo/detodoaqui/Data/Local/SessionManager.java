e6e4a30846581cd26401cb32cb6407fc643f8066
BillyKentAC <billykentac@gmail.com>
2019-05-16 23:07:03 -0500

    cambios al session manager, agregación de Keys para username,password y jwt

--- app/src/main/java/com/miedo/detodoaqui/Data/Local/SessionManager.java
+++ app/src/main/java/com/miedo/detodoaqui/Data/Local/SessionManager.java
@@ -4,30 +4,40 @@ import com.miedo.detodoaqui.Data.User;
 
 public class SessionManager {
 
+    // String keys
+    public static final String USERNAME = "username";
+    public static final String PASSWORD = "password";
+
+    public static final String ISLOGGED = "islogged";
+    public static final String JWT = "jwt";
+
     //Singleton
     private static SessionManager _instance = null;
-    public static SessionManager getInstance(){
+
+    public static SessionManager getInstance() {
         if (_instance == null) {
             _instance = new SessionManager();
         }
         return _instance;
     }
-    private SessionManager(){ }
+
+    private SessionManager() {
+    }
     //SingletonEnd
 
     private User activeUser;
 
-    private void LookForStoredSession(){
+    private void LookForStoredSession() {
 
     }
 
-    public void StartSession(User user){
+    public void StartSession(User user) {
         activeUser = user;
         //Start session
     }
 
-    public void CloseSession(){
-        if(activeUser != null){
+    public void CloseSession() {
+        if (activeUser != null) {
             //Close session
         }
     }
